763497fd5bdfa820629403a656c8ec44f0d24f8f
Mikaela Patella <m.c.patella@gmail.com>
2017-11-22 15:24:27 -0800

    fix redirects, clean logs, and run concurrent independent keys

--- arangodb/src/jepsen/arangodb.clj
+++ arangodb/src/jepsen/arangodb.clj
@@ -49,7 +49,7 @@
   (reify db/DB
     (setup! [_ test node]
       (c/su
-       (info node "installing arangodb" version)
+       (info node "Installing arangodb" version)
 
        ;; Deps
        (c/exec :apt-get :install "-y" "-qq" (str "libjemalloc1"))
@@ -79,49 +79,48 @@
                :> "/etc/arangodb3/arangod.conf")
        (c/exec :service :arangodb3 :start)
 
+       ;; Give cluster time to stabalize. Anything under 30s seems to risk hitting 503s
        (Thread/sleep 30000)))
 
     (teardown! [_ test node]
-      (info node "tearing down arangodb agency")
+      (info node "Tearing down arangodb")
       (c/su
-       (info node "stopping service arangodb3")
+       (info node "Stopping service arangodb3")
        (c/exec :service :arangodb3 :stop)
-       (info node "nuking arangodb3 directory")
+       (info node "Clearing data directory")
        (c/exec :rm :-rf :/var/lib/arangodb3)
        (c/exec :mkdir :/var/lib/arangodb3)
        (c/exec :chown :-R :arangodb :/var/lib/arangodb3)
-       (c/exec :chgrp :-R :arangodb :/var/lib/arangodb3)))
+       (c/exec :chgrp :-R :arangodb :/var/lib/arangodb3)
+       (info node "Clearing log directory")
+       (c/exec :rm :-rf :/var/log/arangodb3)))
 
     db/LogFiles
     (log-files [_ test node]
       ["/var/log/arangodb3/arangod.log"])))
 
-(defn r   [_ _] {:type :invoke, :f :read})
-(defn w   [_ _] {:type :invoke, :f :write, :value (rand-int 5)})
-(defn cas [_ _] {:type :invoke, :f :cas, :value [(rand-int 5) (rand-int 5)]})
-
 (def http-opts {:conn-timeout 5000
                 :content-type :json
-                :follow-redirects true
-                :force-redirects true
+                :trace-redirects true
+                :redirect-strategy :lax
                 :socket-timeout 5000})
 
 (defn agency-read! [node key]
-  (let [bbody (json/generate-string [[key]])
-        url   (client-url node "read")]
-    (http/post url (assoc http-opts :body bbody))))
+  (let [url   (client-url node "read")
+        body (json/generate-string [[key]])]
+    (http/post url (assoc http-opts :body body))))
 
 (defn agency-write! [node key val]
-  (let [bbody (json/generate-string [[{(str key) val}]])
-        url   (client-url node "write")]
-    (http/post url (assoc http-opts :body bbody))))
+  (let [url   (client-url node "write")
+        body (json/generate-string [[{(str key) val}]])]
+    (http/post url (assoc http-opts :body body))))
 
 (defn agency-cas!
   [node key old new]
-  (let [k       (keyword key)
+  (let [url     (client-url node "write")
+        k       (keyword key)
         command [[{k new} {k old}]]
-        body    (json/generate-string command)
-        url     (client-url node "write")]
+        body    (json/generate-string command)]
     (http/post url (assoc http-opts :body body))))
 
 (defn read-parse [resp]
@@ -136,38 +135,33 @@
 (defn precond-fail?   [e] (->> e .getMessage (re-find #"412") some?))
 (defn read-timed-out? [e] (->> e .getMessage (re-find #"Read timed out") some?))
 
-;; Flesh out the op responses here
 (defrecord CASClient [node]
   client/Client
   (open! [this test node]
     (assoc this :node (name node)))
 
   (invoke! [this test op]
-    (let [k     "/jepsen"
+    (let [[k v] (:value op)
           crash (if (= :read (:f op)) :fail :info)
           node  (:node this)]
       (try
         (case (:f op)
-          :read  (let [res (agency-read! node k)
-                       val (read-parse res)]
-                   (case (:status res)
-                     200 (assoc op :type :ok :value val)
-                     307 (assoc op :type crash :error :redirect-loop)))
+          ;; FIXME What to do with read nils
+          :read  (let [res (agency-read! node (str "/" k))
+                       v   (read-parse res)]
+                   (when-not v (warn :read v))
+                   (assoc op :type :ok :value (independent/tuple k v)))
 
-          ;; FIXME Surely not all of these writes are successful
-          :write (let [value (:value op)
-                       ok? (agency-write! node k value)]
-                   (assoc op :type (if ok? :ok :fail)))
+          :write (let [res (agency-write! node (str "/" k) v)]
+                   (assoc op :type :ok))
 
-          :cas   (let [[value value'] (:value op)
-                       res (agency-cas! node k value value')]
-                   (case (:status res)
-                     200 (assoc op :type :ok)
-                     307 (assoc op :type crash :error :redirect-loop))))
+          :cas   (let [[v v'] v
+                       res    (agency-cas! node (str "/" k) v v')]
+                   (assoc op :type :ok)))
 
         (catch Exception e
           (cond
-            (precond-fail?   e) (assoc op :type :fail :error :precondition-fail)
+            (precond-fail?   e) (assoc op :type :fail)
             (unavailable?    e) (assoc op :type crash :error :node-unavailable)
             (read-timed-out? e) (assoc op :type crash :error :read-timed-out)
             :else               (do (error (pr-str (.getMessage e)))
@@ -175,37 +169,46 @@
 
   ;; HTTP clients are not stateful
   (close! [_ _])
-  (setup! [this test])
-  (teardown! [_ test]))
+  (setup! [_ _])
+  (teardown! [_ _]))
 
-(defn client
-  "A compare and set register built around a single consul node."
-  [node]
-  (CASClient. node))
+(defn client [node] (CASClient. node))
 
-;; TODO Add concurrency scaling
+(defn r   [_ _] {:type :invoke, :f :read, :value nil})
+(defn w   [_ _] {:type :invoke, :f :write, :value (rand-int 5)})
+(defn cas [_ _] {:type :invoke, :f :cas, :value [(rand-int 5) (rand-int 5)]})
+
 (defn arangodb-test
   [opts]
+  (info :opts opts)
   (merge tests/noop-test
-         opts
          {:name    "arangodb"
           :os      debian/os
           :db      (db "3.2.7")
           :client  (client nil)
           :nemesis (nemesis/partition-random-halves)
-          :generator (->> (gen/mix [r w cas])
-                          (gen/stagger 1)
+          :model   (model/cas-register)
+          :checker (checker/compose
+                    {:perf  (checker/perf)
+                     :indep (independent/checker
+                             (checker/compose
+                              {:timeline (timeline/html)
+                               :linear   (checker/linearizable)}))})
+          :generator (->> (independent/concurrent-generator
+                           10
+                           (range)
+                           (fn [k]
+                             (->> (gen/mix     [r w cas])
+                                  (gen/stagger 1/30)
+                                  (gen/limit   300))))
                           (gen/nemesis
-                           (gen/seq
-                            (cycle [(gen/sleep 5)
-                                    {:type :info, :f :start}
-                                    (gen/sleep 5)
-                                    {:type :info, :f :stop}])))
-                          (gen/time-limit 30))
-          :model   (model/cas-register 0)
-          :checker (checker/compose {:perf     (checker/perf)
-                                     :timeline (timeline/html)
-                                     :linear   (checker/linearizable)})}))
+                           (gen/seq (cycle [(gen/sleep 5)
+                                            {:type :info, :f :start}
+                                            (gen/sleep 5)
+                                            {:type :info, :f :stop}])))
+                          (gen/time-limit (:time-limit opts)))}
+
+         opts))
 
 (defn -main
   "Handles command line arguments. Can either run a test, or a web server for
