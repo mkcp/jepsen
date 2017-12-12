f779297e629e45a75849f585be5bdefcacace68c
Mikaela Patella <m.c.patella@gmail.com>
2017-12-11 17:44:04 -0800

    add with-errors macro, add doc-http-revision test, and various refactors

--- arangodb/src/jepsen/arangodb.clj
+++ arangodb/src/jepsen/arangodb.clj
@@ -105,27 +105,41 @@
 
 ;; HTTP exception classifiers
 
-;; TODO with-errors macro for http client invokes
-(defn not-found?      [e] (->> e .getMessage (re-find #"404") some?))
-(defn already-exists? [e] (->> e .getMessage (re-find #"409") some?))
-(defn precond-fail?   [e] (->> e .getMessage (re-find #"412") some?))
-(defn internal-error? [e] (->> e .getMessage (re-find #"500") some?))
-(defn unavailable?    [e] (->> e .getMessage (re-find #"503") some?))
-(defn read-timed-out? [e] (->> e .getMessage (re-find #"Read timed out") some?))
+(defn invalid-body?     [e] (->> e .getMessage (re-find #"400") some?))
+(defn not-found?        [e] (->> e .getMessage (re-find #"404") some?))
+(defn already-exists?   [e] (->> e .getMessage (re-find #"409") some?))
+(defn precond-fail?     [e] (->> e .getMessage (re-find #"412") some?))
+(defn internal-error?   [e] (->> e .getMessage (re-find #"500") some?))
+(defn unavailable?      [e] (->> e .getMessage (re-find #"503") some?))
+(defn client-timed-out? [e] (->> e .getMessage (re-find #"Read timed out") some?))
+
+(defmacro with-errors
+  [op & body]
+  `(try
+     ~@body
+     (catch java.net.SocketTimeoutException e#
+       (let [crash# (if (= :read (:f ~op)) :fail :info)]
+         (assoc ~op :type crash# :error :timeout)))
+     (catch Exception e#
+       (let [crash# (if (= :read (:f ~op)) :fail :info)]
+         (cond
+           (not-found?        e#) (assoc ~op :type :fail  :error :key-not-found)
+           (already-exists?   e#) (assoc ~op :type :fail  :error :already-exists)
+           (precond-fail?     e#) (assoc ~op :type :fail  :error :precondition-fail)
+           (invalid-body?     e#) (assoc ~op :type :fail  :error :invalid-body)
+           (internal-error?   e#) (assoc ~op :type crash# :error :server-500)
+           (unavailable?      e#) (assoc ~op :type crash# :error :node-unavailable)
+           (client-timed-out? e#) (assoc ~op :type crash# :error :client-timeout)
+           :else                  (do (error (pr-str (.getMessage e#)))
+                                      (assoc ~op :type crash# :error :indeterminate)))))))
 
 ;;;;;
 
-(defn r   [_ _] {:type :invoke :f :read  :value nil})
-(defn w   [_ _] {:type :invoke :f :write :value (rand-int 5)})
-(defn cas [_ _] {:type :invoke :f :cas   :value [(rand-int 5) (rand-int 5)]})
-
-;;;;;
-
-(def http-opts {:conn-timeout 5000
-                :content-type :json
-                :trace-redirects true
-                :redirect-strategy :lax
-                :socket-timeout 5000})
+(def agency-opts {:conn-timeout 5000
+                  :content-type :json
+                  :trace-redirects true
+                  :redirect-strategy :lax
+                  :socket-timeout 5000})
 
 (defn agency-url
   ([node]   (agency-url node nil))
@@ -134,19 +148,19 @@
 (defn agency-read! [node key]
   (let [url  (agency-url node "read")
         body (json/generate-string [[key]])]
-    (http/post url (assoc http-opts :body body))))
+    (http/post url (assoc agency-opts :body body))))
 
 (defn agency-write! [node key val]
   (let [url  (agency-url node "write")
         body (json/generate-string [[{(str key) val}]])]
-    (http/post url (assoc http-opts :body body))))
+    (http/post url (assoc agency-opts :body body))))
 
 (defn agency-cas!
   [node key old new]
   (let [url     (agency-url node "write")
         command [[{key new} {key old}]]
         body    (json/generate-string command)]
-    (http/post url (assoc http-opts :body body))))
+    (http/post url (assoc agency-opts :body body))))
 
 (defn parse-agency-read [resp]
   (-> resp
@@ -162,10 +176,8 @@
     (assoc this :node (name node)))
 
   (invoke! [this test op]
-    (let [[k v] (:value op)
-          crash (if (= :read (:f op)) :fail :info)
-          node  (:node this)]
-      (try
+    (with-errors op
+      (let [[k v] (:value op)]
         (case (:f op)
           :read  (let [res (agency-read! node (str "/" k))
                        v   (parse-agency-read res)]
@@ -178,15 +190,7 @@
 
           :cas   (let [[v v'] v
                        res    (agency-cas! node (str "/" k) v v')]
-                   (assoc op :type :ok)))
-
-        (catch Exception e
-          (cond
-            (precond-fail?   e) (assoc op :type :fail)
-            (unavailable?    e) (assoc op :type crash :error :node-unavailable)
-            (read-timed-out? e) (assoc op :type crash :error :read-timed-out)
-            :else               (do (error (pr-str (.getMessage e)))
-                                    (assoc op :type crash :error :indeterminate)))))))
+                   (assoc op :type :ok))))))
 
   ;; HTTP clients are not stateful
   (close!    [_ _])
@@ -197,39 +201,27 @@
 
 ;;;;;
 
-(def doc-opts {:conn-timeout 30000
+(def doc-opts {:conn-timeout 20000
                :content-type :json
                :trace-redirects true
                :redirect-strategy :lax
-               :socket-timeout 30000})
+               :socket-timeout 20000})
 
-;; FIXME condense these functions
 (defn doc-url
   ([node] (str "http://" node ":" db-port "/_api/document/jepsen/"))
   ([node k] (str "http://" node ":" db-port "/_api/document/jepsen/" k)))
 
-(defn all-keys-url [node]
-  (str "http://" node ":" db-port "/_api/simple/all-keys"))
-
-(defn all-docs-url [node]
-  (str "http://" node ":" db-port "/_api/simple/all"))
-
-(defn collection-url [node]
-  (str "http://" node ":" db-port "/_api/collection/"))
-
-(defn admin-url [node]
-  (str "http://" node ":" db-port "/_admin/"))
-
 (defn doc-setup-collection! [test node]
-  (let [body (json/generate-string {:name              "jepsen"
+  (let [url (str "http://" node ":" db-port "/_api/collection/")
+        body (json/generate-string {:name              "jepsen"
                                     :waitForSync       true
                                     :numberOfShards    (count (:nodes test))
                                     :replicationFactor (count (:nodes test))})]
-    (http/post (collection-url node) (assoc doc-opts :body body))))
+    (http/post url (assoc doc-opts :body body))))
 
 (defn init-keyspace!
   "Creates keys with value 0 in our collection. The number of keys is 10 times the
-  concurrency value passed to the test. This isn't perfect because very long tests might
+  concurrency val on the test. This isn't perfect because very long tests might
   go past and run into errors."
   [test node]
   (let [max (* 10 (:concurrency test))
@@ -238,9 +230,6 @@
         body (json/generate-string t)]
     (http/post (doc-url node) (assoc doc-opts :body body))))
 
-(defn cluster-test! [node]
-  (http/get (str (admin-url node) "cluster_test")))
-
 (defn doc-read! [node k]
   (let [url (doc-url node k)]
     (http/get url doc-opts)))
@@ -248,15 +237,13 @@
 (defn doc-write! [node k v]
   (let [url  (doc-url node)
         body (json/generate-string [{:_key (str k) :value v}])]
-    (http/put url (assoc doc-opts
-                         :body body
-                         :query-params {:waitForSync true}))))
+    (http/put url (assoc doc-opts :body body))))
 
 ;; TODO Get _rev from read, write If-Match _rev
 (defn doc-cas! [node k v v']
   (let [url  (doc-url node)
         body (json/generate-string [{k v} {k v'}])]
-    (http/post url (assoc doc-opts :body body))))
+    (http/put url (assoc doc-opts :body body))))
 
 (defn parse-doc-read [res]
   (-> res :body json/parse-string (get "value")))
@@ -279,33 +266,17 @@
           (throw e)))))
 
   (invoke! [this test op]
-    (let [[k v] (:value op)
-          crash (if (= :read (:f op)) :fail :info)
-          node  (:node this)]
-      (try
+    (with-errors op
+      (let [[k v] (:value op)]
         (case (:f op)
           :read (let [res (doc-read! node k)
                       v   (parse-doc-read res)]
                   (assoc op :type :ok :value (independent/tuple k v)))
 
-          :write (let [res (doc-write! node k v)
-                       err (-> res :headers (get "X-Arango-Error-Codes"))]
-                   (if-not err
-                     (assoc op :type :ok)
-                     (assoc op :type crash :error (pr-str err))))
+          :write (let [res (doc-write! node k v)]
+                   (assoc op :type :ok))
 
-          :cas (assoc op :type :fail :error :not-implemented))
-
-        (catch java.net.SocketTimeoutException e
-          (assoc op :type crash :error :timeout))
-
-        (catch Exception e
-          (cond
-            (read-timed-out? e) (assoc op :type crash :error :read-timed-out)
-            (not-found? e)      (assoc op :type crash :error :not-found)
-            (internal-error? e) (assoc op :type crash :error :server-500)
-            :else               (do (error (pr-str (.getMessage e)))
-                                    (assoc op :type crash :error :indeterminate)))))))
+          :cas (assoc op :type :fail :error :not-implemented)))))
 
   ;; HTTP clients are not stateful
   (close! [_ _])
@@ -313,24 +284,18 @@
 
 (defn document-register-client [node] (DocumentClient. node))
 
-(defn doc-read-all! [node]
-  (let [url  (all-docs-url node)
+(defn read-all! [node]
+  (let [url  (str "http://" node ":" db-port "/_api/simple/all")
         body (json/generate-string {:collection "jepsen"
                                     :batchSize 50000})]
     (http/put url (assoc doc-opts :body body))))
 
-(defn admin-url [node]
-  (str "http://" node ":" db-port "/_admin/"))
-
-(defn parse-int [s] (Integer. (re-find  #"\d+" s )))
-
-(defn parse-read-all [res]
+(defn read-all->ints
+  "Takes an http response from a full and produces a sequence of Arango document keys"
+  [res]
   (let [result (-> res :body json/parse-string (get "result"))]
     (map #(Integer. (get % "_key")) result)))
 
-;; FIXME Not a valid test if the keyspace exceeds 1000. Adjusting the limit doesn't increase entries past 1k
-;;       but it looks like it might return a cursor, so if the read client can traverse that and merge the
-;;       return set then we might have a stew baby
 (defrecord DocumentSetClient [node]
   client/Client
   (open! [this test node]
